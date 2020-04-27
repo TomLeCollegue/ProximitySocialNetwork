4ef8cda6564bbdf05d0e66d45e859cd6ad6e6235
laurent cutting <laurent.cutting@gmail.com>
2020-04-27 20:30:29 +0200

    permission not finished

--- app/src/main/java/com/example/proximitysocialnetwork/AccountCreationActivity.java
+++ app/src/main/java/com/example/proximitysocialnetwork/AccountCreationActivity.java
@@ -1,20 +1,29 @@
 package com.example.proximitysocialnetwork;
 
+import androidx.annotation.CallSuper;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class AccountCreationActivity extends AppCompatActivity {
 
+    private static final String TAG = "test";
     private EditText name;
     private EditText email;
     private EditText birthDate;
@@ -30,6 +39,7 @@ public class AccountCreationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_creation);
+
 
         // ************ link id to view **********************************
         name = (EditText) findViewById(R.id.name);
@@ -54,7 +64,6 @@ public class AccountCreationActivity extends AppCompatActivity {
 
                     Intent intent = new Intent(AccountCreationActivity.this, MainActivity.class);
                     startActivity(intent);
-                    finish();
                 }
             }
         });
@@ -62,10 +71,51 @@ public class AccountCreationActivity extends AppCompatActivity {
         profileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openGallery();
+                if(isStoragePermissionGranted()==false)  openGallery();
             }
         });
 
+    }
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
+    /*
+            * Request needed permissions to user through UI
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @CallSuper
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode != AccountCreationActivity.()) {
+            return;
+        }
+
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this,"Permissions manquantes", Toast.LENGTH_LONG).show();
+                finish();
+                return;
+            }
+        }
+        recreate();
     }
 
     private void openGallery() {
